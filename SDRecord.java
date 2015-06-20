649abca06e159d2cf6294d721c59aba720dc00f4
Andrea Dari <andreadari91@gmail.com>
2015-06-20 10:43:20 +0200

    Added function to print how many bytes are transferred

--- SDRecord.java
+++ SDRecord.java
@@ -1,3 +1,23 @@
+//########
+//#LICENSE                                                   
+//########
+
+//# Tool for audio recording with SDR. Please visit the project's website at: https://github.com/cybernova/SDRecord
+//# Copyright (C) 2015 Andrea 'cybernova' Dari (andreadari91@gmail.com)                                   
+//#                                                                                                       
+//# This shell script is free software: you can redistribute it and/or modify                             
+//# it under the terms of the GNU General Public License as published by                                   
+//# the Free Software Foundation, either version 2 of the License, or                                     
+//# any later version.                                                                   
+//#                                                                                                       
+//# This program is distributed in the hope that it will be useful,                                       
+//# but WITHOUT ANY WARRANTY; without even the implied warranty of                                        
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                         
+//# GNU General Public License for more details.                                                          
+//#                                                                                                       
+//# You should have received a copy of the GNU General Public License                                     
+//# along with this application.  If not, see <http://www.gnu.org/licenses/>.
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -8,13 +28,14 @@ import java.net.UnknownHostException;
 
 public class SDRecord {
 
-	//ARGS -> [1]: minutes to record [2]: source port  [3]: host [4]: destination port [5]: buffer size
+	//ARGS -> [1]: minutes to record [2]: source port  [3]: destination host [4]: destination port [5]: buffer size
 	
 	public static void main(String[] args) {
 		
 		boolean deamon = false;
 		long recordTo = 0;
 		int sourcePort = 0, destPort = 0;
+		long txsize = 0;
 		InetAddress host = null;
 		//Default buffer size
 		int buffSize = 1500;
@@ -53,7 +74,7 @@ public class SDRecord {
 		{	
 			packet.setData(buffer);
 			try { socket.receive(packet); } //catch (SocketTimeoutException e) { System.out.println("Timeout reached"); socket.close(); System.exit(5); }
-											catch (IOException e) { e.printStackTrace(); System.exit(6); }
+			catch (IOException e) { e.printStackTrace(); System.exit(6); }
 			//selecting packets that contains information
 			for (byte b : packet.getData())
 				if ( b != 0) 
@@ -61,11 +82,19 @@ public class SDRecord {
 					packet.setPort(destPort);
 					packet.setAddress(host);
 					try { socket.send(packet); } catch (IOException e) { e.printStackTrace(); System.exit(7); }
+					txsize = txsize + packet.getLength();
+					System.out.print("\r"+formatSize(txsize)+" transferred"+"\033[K");
 					break;
 				}
 		}
 		//closing socket and exit
 		socket.close();
 		System.exit(0);
+	}
+	
+	private static String formatSize(long v) {
+	    if (v < 1024) return v + " B";
+	    int z = (63 - Long.numberOfLeadingZeros(v)) / 10;
+	    return String.format("%.2f %sB", (double)v / (1L << (z*10)), " KMGTPE".charAt(z));
 	}
 }
