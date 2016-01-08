7b1f8bc2da288e1b96a6f38a62707b7e64be9e2a
Andrea Dari <andrea@Workstation>
2016-01-08 18:30:06 +0100

    Version 1.0 released

--- SDRecord.java
+++ SDRecord.java
@@ -2,10 +2,10 @@
 //#LICENSE                                                   
 //########
 
-//# Tool for audio recording with SDR. Please visit the project's website at: https://github.com/cybernova/SDRecord
-//# Copyright (C) 2015 Andrea 'cybernova' Dari (andreadari91@gmail.com)                                   
+//# Tool for audio recording with SDR v1.0. Please visit the project's website at: https://github.com/cybernova/SDRecord
+//# Copyright (C) 2016 Andrea 'cybernova' Dari (andreadari91@gmail.com)                                   
 //#                                                                                                       
-//# This software is free software: you can redistribute it and/or modify                             
+//# This shell script is free software: you can redistribute it and/or modify                             
 //# it under the terms of the GNU General Public License as published by                                   
 //# the Free Software Foundation, either version 2 of the License, or                                     
 //# any later version.                                                                   
@@ -18,83 +18,196 @@
 //# You should have received a copy of the GNU General Public License                                     
 //# along with this application.  If not, see <http://www.gnu.org/licenses/>.
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-//import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import org.apache.commons.cli.*;
 
 public class SDRecord {
-
-	//ARGS -> [1]: minutes to record [2]: source port  [3]: destination host [4]: destination port [5]: buffer size
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		
-		boolean deamon = false;
-		long recordTo = 0;
+		boolean recordToInf = false;
+		long recordTo = 0, txsize = 0, wr = 0, max = 0;
 		int sourcePort = 0, destPort = 0;
-		long txsize = 0;
-		InetAddress host = null;
-		//Default buffer size
-		int buffSize = 1500;
+		String val;
+		OutputStream writer = null;
+		InetAddress rhost = null, lhost = null;
 		DatagramSocket socket = null;
+		
+		//Default values
+		int buffSize = 1500;
+		try { lhost = InetAddress.getByName("0.0.0.0"); } 
+		catch (UnknownHostException e1) { System.err.println("Host not reconized"); e1.printStackTrace(); System.exit(3); }
+		recordToInf = true;
+		sourcePort = 7355;
+		
+		Options options = new Options();
+		
+		options.addOption("m", true, "Minutes to record, default is no limit");
+		options.addOption("l", true, "Bind to a specific local address, default is 0.0.0.0");
+		options.addOption("p", true, "Local port to use, default is 7355");
+		options.addOption("r", true, "Remote address where to send data");
+		options.addOption("d", true, "Remote port, to use with -r option");
+		options.addOption("f", true, "Output file where to save the recording");
+		options.addOption("s", true, "Stop recording when reaching specified MBs");
+		options.addOption("h", false, "Help");
+		
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = null;
+		try {
+			cmd = parser.parse(options,args);
+		} catch (ParseException e1) {
+			System.err.println("Error while parsing the command line");
+			System.exit(1);
+		}
+		
+		if (cmd.hasOption("m"))
+		{
+			val = cmd.getOptionValue("m");
+			try {
+				if (Long.parseLong(val) < 0 )
+				{
+					System.err.println("-m argument value cannot be negative");
+					System.exit(3);
+				}
+				recordTo = System.currentTimeMillis() + (Long.parseLong(val) * 60000); 
+				recordToInf = false; 
+				}
+			catch (NumberFormatException e ) { System.err.println("-m argument not an integer"); e.printStackTrace(); System.exit(3); }
+		}
+		
+		if (cmd.hasOption("l"))
+		{
+			val = cmd.getOptionValue("l");
+			try { lhost = InetAddress.getByName(val); }
+			catch (UnknownHostException e ) { System.err.println("Host not reconized"); e.printStackTrace(); System.exit(3); }
+		} 
+		
+		if (cmd.hasOption("p"))
+		{
+			val = cmd.getOptionValue("p");
+			try { sourcePort = Integer.parseInt(val); }
+			catch (NumberFormatException e ) { System.err.println("-p argument not an integer"); e.printStackTrace(); System.exit(3); }
+		}
 
-		//Checking the number of arguments
-		if (args.length != 4 && args.length != 5)
-		{ System.out.println("Usage: SDRecord minutes_to_record source_port host dest_port [buff_size]"); System.exit(1); }
+		if (cmd.hasOption("r"))
+		{
+			val = cmd.getOptionValue("r");
+			try { rhost = InetAddress.getByName(val); }
+			catch (UnknownHostException e ) { System.err.println("Host not reconized"); e.printStackTrace(); System.exit(3); }
+		}
+		
+		if (cmd.hasOption("d"))
+		{
+			val = cmd.getOptionValue("d");
+			try { destPort = Integer.parseInt(val); }
+			catch (NumberFormatException e ) { System.err.println("-d argument not an integer"); e.printStackTrace(); System.exit(3); }
+		}
+		
+		if (cmd.hasOption("f"))
+		{
+			val = cmd.getOptionValue("f");
+			try { writer = new FileOutputStream(val); }
+			catch (FileNotFoundException e ) { System.err.println("File not found"); e.printStackTrace(); System.exit(3); }
+		}
+		
+		if (cmd.hasOption("s"))
+		{
+			val = cmd.getOptionValue("s");
+			if (Long.parseLong(val) < 0 )
+			{
+				System.err.println("-s argument value cannot be negative");
+				System.exit(3);
+			}
+			try { max = Long.parseLong(val) * 1024 * 1000; }
+			catch (NumberFormatException e ) { System.err.println("-s argument not an integer"); e.printStackTrace(); System.exit(3); }
+		}
+		
+		if (cmd.hasOption("h"))
+		{
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp( "SDRecord", options );
+			System.exit(0);
+		}
 		
 		try {
-		if (Integer.parseInt(args[0]) != 0)
-			//current time + minutes to record (ideal)
-			recordTo = System.currentTimeMillis() + (Integer.parseInt(args[0]) * 60000);
-		else
-			//minutes to record = 0 -> deamon
-			deamon = true;
-		sourcePort = Integer.parseInt(args[1]);
-		host = InetAddress.getByName(args[2]);
-		destPort = Integer.parseInt(args[3]);
-		if (args.length != 4)
-			buffSize = Integer.parseInt(args[4]);
-		} catch (NumberFormatException e) { System.out.println("Usage: SDRecord minutes_to_record source_port host dest_port [buff_size]"); System.exit(2); } 
-		  catch (UnknownHostException e) { System.out.println("Host not reconized"); System.exit(3); }
-		
-		try {
-			socket = new DatagramSocket(sourcePort);
+			socket = new DatagramSocket(sourcePort, lhost);
 			//socket options
 			socket.setReuseAddress(true);
-			//socket.setSoTimeout(60000);
-		} catch (SocketException e) { e.printStackTrace(); System.exit(4); }
+		} catch (SocketException e) { e.printStackTrace(); System.exit(3); }
 		
 		byte[] buffer = new byte[buffSize];			
 		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 		
-		while ( deamon == true  || System.currentTimeMillis() <= recordTo ) 
+		System.err.println("Listening " + lhost.toString() + " on port " + sourcePort);
+			
+		while ( recordToInf == true || System.currentTimeMillis() <= recordTo ) 
 		{	
+			//Stop recording when reaching max bytes
+			if ( max != 0 && txsize >= max )
+				break;
+			
 			packet.setData(buffer);
-			try { socket.receive(packet); } //catch (SocketTimeoutException e) { System.out.println("Timeout reached"); socket.close(); System.exit(5); }
-			catch (IOException e) { e.printStackTrace(); System.exit(6); }
-			//selecting packets that contains information
-			for (byte b : packet.getData())
-				if ( b != 0) 
-				{
-					packet.setPort(destPort);
-					packet.setAddress(host);
-					try { socket.send(packet); } catch (IOException e) { e.printStackTrace(); System.exit(7); }
-					txsize = txsize + packet.getLength();
-					System.out.print("\r"+formatSize(txsize)+" transferred"+"\033[K");
-					break;
-				}
+			try { socket.receive(packet); }
+			catch (IOException e) { e.printStackTrace(); System.exit(4); }
+			
+			//Ignoring packets with no data
+			if (basicFilter(packet) == null)
+				continue;
+			
+			if (writer == null && rhost == null)
+				wr = recordToStdout(packet);
+			if (writer != null) 
+				wr = recordToFile(packet, writer);
+			if (rhost != null)
+				wr = recordToSocket(packet, socket, rhost, destPort);
+			
+			txsize = txsize + wr;
+			System.err.print("\r"+formatSize(txsize)+" transferred"+"\033[K"+"\t Press Ctrl+c to terminate");
 		}
 		//closing socket and exit
 		socket.close();
+		System.out.println();
 		System.exit(0);
 	}
 	
-	private static String formatSize(long v) {
+	private static byte[] basicFilter(DatagramPacket packet)
+	{
+		for (byte b : packet.getData())
+			if ( b != 0 )
+			return packet.getData();
+		return null;
+	}
+	
+	private static long recordToStdout(DatagramPacket packet) 
+	{
+		System.out.write(packet.getData(), 0, packet.getLength()); 
+		System.out.flush();
+		return packet.getLength();
+	}
+	private static long recordToFile(DatagramPacket packet, OutputStream writer) 
+	{
+		try { writer.write(packet.getData(), 0, packet.getLength()); writer.flush(); } catch (IOException e) { e.printStackTrace(); System.exit(5); }
+		return packet.getLength();
+	}
+	private static long recordToSocket(DatagramPacket packet, DatagramSocket socket, InetAddress rhost, int destPort)
+	{
+		packet.setPort(destPort);
+		packet.setAddress(rhost);
+		try { socket.send(packet); } catch (IOException e) { e.printStackTrace(); System.exit(5); }
+		return packet.getLength();
+	}
+	private static String formatSize(long v) 
+	{
 	    if (v < 1024) return v + " B";
 	    int z = (63 - Long.numberOfLeadingZeros(v)) / 10;
-	    return String.format("%.2f %sB", (double)v / (1L << (z*10)), " KMGTPE".charAt(z));
+	    return String.format("%.1f %sB", (double)v / (1L << (z*10)), " KMGTPE".charAt(z));
 	}
 }
